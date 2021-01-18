2d70e065cbf1bd18461c040685259508ad395a2a
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-01-18 15:06:10 -0600

    tipo de variable Var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,21 @@
+//Iniciar la variable
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+//Las variables se puede reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+//JavaScript es un lenguaje dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+//se puede inicializar sin valor y asignarlos despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//inicializar multiples variables
+var categoria = 'Computadora',
+    marca = "Marca famosa",
+    calificacion = 5;
+
