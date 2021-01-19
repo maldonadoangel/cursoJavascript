32453c60578b8c20e213b8a3c08f336ae63f8bba
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-01-19 09:19:16 -0600

    Todo sobre la variable let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,12 @@
+let productos = 'Tablet';
+
+//Reasignamos el valor de la variable let
+
+ productos = 'Television';
+
+console.log(productos);
+
+productos = 69;
+console.log(productos);
+productos = true;
+console.log(productos);
