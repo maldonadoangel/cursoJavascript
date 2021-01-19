c494f418814ef66e9a592eb64a70b2974b1e0175
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-01-19 09:24:54 -0600

    Todo sobre la constante

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,7 @@
+const nombre = "Angel";
+
+//No se puede reasignar una constante, por lo tanto es constante a su valor inicial
+//Una constante debe inicializarse siempre, sino se inicializa siempre presentara un error
+// nombre = "Hernan";
+
+console.log(nombre);
