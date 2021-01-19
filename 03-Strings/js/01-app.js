d8e129b083895133651cacead3e4232bf73f9f1c
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-01-19 09:59:42 -0600

    Crear un String en JavaScript

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,6 @@
+const producto = "Television de 20\" pulgadas";
+const producto2 = String("Telivision de 35 pulgadas");
+const producto3 = new String("Monitor de 55 pulgadas");
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
