484704f10a6f7a826845216e48282376a84b083e
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-01-19 10:09:37 -0600

    Strings .includes y .length

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,12 @@
+const producto = 'Television de 20 pulgadas';
+
+console.log(producto);
+//Conocer la cantidad de letras en la cadena de texto 
+console.log(producto.length);
+
+// console.log(producto.indexOf('pulgadas'));
+
+console.log(producto.includes('pulgadas'));
+console.log(producto.includes('Pulgadas'));
+
+
