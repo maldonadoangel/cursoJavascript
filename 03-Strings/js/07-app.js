34a6a84551fd3cf613f1e23b68530141ac8f4ceb
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-02-10 09:58:28 -0600

    convertir texto en mayusculas y minusculas

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,19 @@
+const producto = 'Monitor 20 pulgadas';
+
+// toUpperCase sirve para poner en mayusculas el texto
+console.log(producto.toUpperCase());
+
+//toLowerCase sirve para poner todo en minusculas
+//Ayuda a la hora de que las personas ingresan en un formulario su correo, aunque ingresen en mayuscula su correo
+//lo convertira todo en minusculas
+console.log(producto.toLowerCase());
+
+//ejemplo del correo
+
+const correo = 'ANGELMORALES@GMAIL.COM';
+
+console.log(correo.toLowerCase());
+
+//para convertir numeros en string
+const precio = 200;
+console.log(precio.toString());
