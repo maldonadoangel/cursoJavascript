02b7c3d065626ddcee0101410bbbc9c700e7493d
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-02-10 22:26:28 -0600

    objeto Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,51 @@
+let resultado;
+
+//PI    
+
+resultado = Math.PI;
+
+
+//Redondear
+
+resultado = Math.round(2.8);
+resultado = Math.round(2.4);
+
+
+//redondear hacia arriba
+
+resultado = Math.ceil(2.1);
+
+//redondear hacia abajo
+resultado = Math.floor(2.9);
+
+//Raiz cuadrada
+
+resultado = Math.sqrt(144);
+
+//Absoulto
+
+
+resultado = Math.abs(-500);
+
+//Potencia
+
+resultado = Math.pow(2, 4);
+
+//Minimo
+
+resultado = Math.min(3, 4, 5, 10,1, 0, -10);
+
+
+//maximo
+resultado = Math.max(3, 4, 5, 10,1, 0, -10);
+
+
+//aleatorio
+resultado = Math.random();
+
+//aleatorio dentro de un rango
+
+resultado = Math.floor(Math.random() * 30);
+
+
+console.log(resultado);
