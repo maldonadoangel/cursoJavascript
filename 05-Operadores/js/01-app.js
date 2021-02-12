e0d8eb2b99bd0f6295f3b1357f88f10255908762
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-02-11 18:53:00 -0600

    operador mayor y menor

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,13 @@
+const numero1 = 20;
+const numero2 = 30;
+
+
+
+//Nos devuelve un valor de false y uno true con el signo de mayor
+console.log(numero1 > numero2);
+
+console.log(numero2 > numero1);
+
+//Nos devuelve un valor de false y uno true con el signo de mayor
+console.log(numero1 < numero2);
+console.log(numero2 < numero1);
