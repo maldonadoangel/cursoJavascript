ad971c878b61eca42e999e5c20f9cdf275dc1765
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-02-11 18:44:56 -0600

    Convertir variables en enteros y flotantes

---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,18 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = "Uno";
+const numero4 = "20.3";
+
+
+//Como convertir valores en enteros y flotantes
+console.log(Number.parseInt(numero2));
+console.log(numero2);
+console.log(Number.parseFloat(numero4));
+console.log(numero4);
+console.log(Number.parseInt(numero3));
+
+
+
+//COmo saber si es un numero entero o no
+console.log(Number.isInteger(numero4));
+console.log(Number.isInteger(numero1));
