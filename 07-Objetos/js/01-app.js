425ac89cc9223dd2bd5f3e47ed2fb0cb069edb3a
Angel Morales <maldonadoangel7069@gmail.com>
2021-03-03 16:02:06 -0600

    Object literal

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const nombre = 'Monitor de 20 pulgadas';
+const precio = 3000;
+const disponible = false;
+
+//Un objeto agrupa todo en una variable
+//object literal
+const producto = {
+    nombre: 'Monitor de 20 pulgadas',
+    precio: 3000,
+    disponible: true
+}
+
+
+console.log(producto);
