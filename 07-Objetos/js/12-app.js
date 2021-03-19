f4159eef0adf48c054e371ee3ecadbf217f21cf0
Angel Morales <maldonadoangel7069@gmail.com>
2021-03-18 22:06:10 -0600

    Object construcion

--- 07-Objetos/js/12-app.js
+++ 07-Objetos/js/12-app.js
@@ -1,10 +1,21 @@
+/* Object literal */
 const producto = {
     nombre: 'Pc gamer GTX 1050',
     precio: 4000,
-    disponible: true,
-    mostrarInfo: function () {
-        console.log(`EL producto: ${this.nombre} con su precio de: ${this.precio}`);
+    disponible: true
     }
+
+
+    /* Object construcion */
+function Producto(nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = true;
 }
 
-producto.mostrarInfo();
+const producto2 = new Producto('Monitor de 20 pulgadas', 3000);
+console.log(producto2);
+
+const producto3 = new Producto('Celular', 5000);
+console.log(producto3);
+
