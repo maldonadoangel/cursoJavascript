10cdb45f1f0aa1b0a260639f4ed56808af6b75f1
Angel Morales <maldonadoangel7069@gmail.com>
2021-03-20 04:52:39 -0600

    destructuring arreglos

---
+++ 08-Arrays/js/08-app.js
@@ -0,0 +1,21 @@
+const producto ={
+    nombre: 'Monitor de 20 pulgadas',
+    precio: 3000,
+    disponible: true
+
+}
+
+/* Destructuring, lo que hace es sacar de la estructura*/
+const {nombre} = producto;
+
+console.log(nombre);
+
+
+/* Destructuring con arreglos */
+const numeros = [10,20,30,40,50];
+
+const [primero, ...tercero] = numeros;
+
+console.log(primero);
+
+console.log(tercero);
