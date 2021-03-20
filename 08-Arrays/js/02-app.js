9e9ca5baa6e99fd241d901c9bb58010a8c62dc4a
Angel Morales <maldonadoangel7069@gmail.com>
2021-03-20 00:58:20 -0600

    Acceder a valores del arreglo

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,15 @@
+const numeros = [10,20,30,40,50, [1,2,3]];
+
+console.log(numeros);
+
+console.table(numeros);
+
+
+/* Acceder al arreglo */
+
+console.log(numeros[0]);
+console.log(numeros[2]);
+/* Valor que no existe */
+console.log(numeros[30]);
+/* Acceder a un arreglo dentro de otro arreglo */
+console.log(numeros[5][0]);
