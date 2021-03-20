f75796ecb452336d9e4262bd6cda8684ad5bf4d2
Angel Morales <maldonadoangel7069@gmail.com>
2021-03-20 00:54:34 -0600

    Creacion de arreglos

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,23 @@
+/* Un arreglo guarda informacion */
+const numeros = [10, 20, 30];
+
+console.log(numeros);
+/* Otra forma de crear arreglos */
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abrir');
+console.log(meses);
+
+/* objetos */
+const producto ={
+    nombre: 'Celular',
+    precio: 4000,
+    descripcion: 'Samsung s21'
+}
+
+
+console.log(producto);
+
+
+/* Un arreglo que contiene datos de todo tipo */
+
+const todoTipo = ["Hola", 10, true, "si", null, {nombre: 'Angel', trabajo: 'Programador'}, [1,2,3]];
+console.log(todoTipo);
