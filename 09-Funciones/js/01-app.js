ed9ccaa4ddf5609b4531323f43273dfa7550db0b
Angel Morales <maldonadoangel7069@gmail.com>
2021-04-05 00:21:44 -0600

    function de exprecion y de declaracion

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,20 @@
+//Declaracion de funcion (funcion declaration)
+
+function sumar(){
+    console.log(2+2);
+}
+
+sumar();
+sumar();
+sumar();
+
+
+// Expresion de funcion (function expression)
+
+const sumar2 = function(){
+    console.log(3+3);
+}
+
+
+sumar2();
+sumar2();
---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,19 @@
+//Declaracion de funcion (funcion declaration)
+sumar();
+
+function sumar(){
+    console.log(2+2);
+}
+
+
+
+
+// Expresion de funcion (function expression)
+sumar2();
+
+const sumar2 = function(){
+    console.log(3+3);
+}
+
+
+
