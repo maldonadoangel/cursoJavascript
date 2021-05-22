1a892c25bf9e2ea4ab9bf6fb3ea97afa632d6b4f
Angel Morales <maldonadoangel7069@gmail.com>
2021-05-22 17:56:21 -0600

    Estructuras de control

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,7 @@
+const puntaje = 1000;
+
+if(puntaje === 1000){
+    console.log(`Tu puntaje actual es de: ${puntaje}`);
+}else{
+    console.log('Error de puntaje debido a que es menor de 1000');
+}
