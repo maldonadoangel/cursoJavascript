9286e16792b0f71ed7174c3a843d3b9187784afb
Angel Morales <maldonadoangel7069@gmail.com>
2021-05-22 17:40:54 -0600

    Arrow function en reproductor de musica

---
+++ 09-Funciones/js/13-app.js
@@ -0,0 +1,31 @@
+
+//Añadir funciones al objeto
+const nombrePlaylist = 'Maldita Hamburguesa';
+
+const reproductor = {
+    cancion: ' ',
+    reproducir: id =>console.log (`Reproduciendo cancion con el ${id}...`),
+    pausar: () => console.log(`Pausando...`),
+    borrar: id => console.log(`Borrando cancion con el id ${id}`),
+    crearPlaylist: nombre=> console.log(`Se esta creando la Playlist con el nombre: ${nombre}`),
+    reproducirPlaylist: nombre=> console.log(`Reproduciendo la playlist con el nombre de: ${nombre}`),
+    set nuevaCancion(cancion){
+        this.cancion = cancion;
+        console.log(`Añadiendo ${cancion}`);
+},
+    get obtenerCancion(){
+        console.log(`La cancion que se añadio recientemente es: ${this.cancion}`);
+    }
+    
+}
+
+reproductor.nuevaCancion = 'Enter Sadman';
+reproductor.obtenerCancion;
+
+
+reproductor.reproducir(30);
+reproductor.reproducir(20);
+reproductor.pausar();
+reproductor.borrar(30);
+reproductor.crearPlaylist(nombrePlaylist);
+reproductor.reproducirPlaylist(nombrePlaylist);
