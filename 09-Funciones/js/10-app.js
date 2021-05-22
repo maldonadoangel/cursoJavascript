d23a444f4a1fdb68d60440608c5d625efa868c12
Angel Morales <maldonadoangel7069@gmail.com>
2021-05-22 14:22:13 -0600

    Arrow Function

---
+++ 09-Funciones/js/10-app.js
@@ -0,0 +1,11 @@
+//FUncion normal
+const aprendiendo = function(){
+    console.log('aprendiendo Javascript');
+}
+
+
+//Arrow function
+//Si nuestro arrow function es de una linea, de forma automatica puede entender
+//que no es necesario las llaves ni el console log, ahora si es de varias lineas si lleva llaves y parentesis
+const aprendiendo2 = () => 'aprendiendo Javascript';
+console.log(aprendiendo2());
