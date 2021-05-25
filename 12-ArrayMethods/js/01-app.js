5cd1cc401f1848df4c3f3f88a176b0d16d9c6f32
Angel Morales <maldonadoangel7069@gmail.com>
2021-05-25 01:14:06 -0600

    Arrays methods .some()

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -8,3 +8,27 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+
+//COmprobar si un valor existe en un arreglo
+
+meses.forEach((mes)=>{
+    if(mes === 'Enero'){
+        console.log('Enero si existe');
+    }
+
+});
+
+const resultado = meses.includes('Enero');
+
+console.log(resultado);
+
+
+//En un arreglo de objetos se utiliza some, tambien sirve soporta un arreglo normal
+
+const existe = carrito.some((producto)=>{
+    return producto.nombre === 'Celular';
+
+});
+
+console.log(existe);
