3d5e9a9a1246b56f40d1caa05a70bdedb64e5e85
Angel Morales <maldonadoangel7069@gmail.com>
2021-06-14 23:17:39 -0600

    acceder a los elementos id del html

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.getElementById('formulario');
+console.log(formulario);
+
+//No existe
+const noExiste = document.getElementById('No-existe');
+console.log(noExiste);
+
+//Si existen mas de dos formularios, solo tomara en cuenta el primero que encuentre, a diferencia 
+//de className ya que las clases se pueden utilizar mas de una vez por document
