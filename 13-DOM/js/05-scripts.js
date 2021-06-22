ee4cd7c77ed60dbd3b0a136835d4ed6210288f57
Angel Morales <maldonadoangel7069@gmail.com>
2021-06-22 02:38:38 -0600

    querySelectorAll

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,8 @@
+//QuerySelectorAll nos sirve para seleccionar muchas clases que se llaman igual
+
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+//No existe el elemento
+const noExiste = document.querySelectorAll('.noExistes');
+console.log(noExiste);
