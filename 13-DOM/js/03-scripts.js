da32a329c86ea1b5c86e7f97a5a1fe186c4de874
Angel Morales <maldonadoangel7069@gmail.com>
2021-06-22 02:32:34 -0600

    querySelector

--- 13-DOM/js/03-scripts.js
+++ 13-DOM/js/03-scripts.js
@@ -6,4 +6,4 @@ const noExiste = document.getElementById('No-existe');
 console.log(noExiste);
 
 //Si existen mas de dos formularios, solo tomara en cuenta el primero que encuentre, a diferencia 
-//de className ya que las clases se pueden utilizar mas de una vez por document
+//de className ya que las clases se pueden utilizar mas de una vez por document 
---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,19 @@
+const card = document.querySelector('.card');
+console.log(card);
+
+//podemos tener selectores especificos como en css
+
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+//Seleccionar el segundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+//Seleccionar formulario
+const formulario = document.querySelector('#formulario');
+console.log(formulario); 
+
+//Seleccionar elementos HTML
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
