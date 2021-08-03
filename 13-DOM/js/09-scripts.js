e47d88d7b098f233aa74bab12a3c30636e5e932f
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-02 22:31:58 -0600

    eliminando elementos en el DOM

---
+++ 13-DOM/js/09-scripts.js
@@ -0,0 +1,12 @@
+/* const navegacion = document.querySelector('.navegacion');
+console.log(navegacion)
+console.log(navegacion.childNodes); //Los espacios en blanco son considerados elementos
+console.log(navegacion.children); //te lista elementos reales
+
+console.log(navegacion.children[0]); */
+
+/* Eliminar elemntos con .remove */
+const primerEnlace = document.querySelector('a');
+primerEnlace.remove();
+
+console.log("hola");
